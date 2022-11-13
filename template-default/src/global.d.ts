57b15efcfe69f142758b58e0250c2b46399a6829
Masood Abdolian <m.abdolian@gmail.com>
2022-11-13 16:57:39 +0330

    added part declaration

--- template-default/src/global.d.ts
+++ template-default/src/global.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+import '@types/react';
+
+declare module 'react' {
+  interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
+    part?: string;
+  }
+}
