826151cd92f1524c0fba060074bc9cf0f6cce536
Masood Abdolian <m.abdolian@gmail.com>
2024-07-23 19:08:19 +0330

    refactor: In order to improve build and start performance, migrate logics from Rollup to Vite.
    BREAKING CHANGE: The logic was migrated from Rollup to Vite to improve build and startup performance.

---
+++ template-default/README.md
@@ -0,0 +1,31 @@
+# Starter
+
+A minimal starting point for creating a custom element package.
+
+## Getting Started
+
+Install the dependencies via [npm](https://npmjs.org):
+
+```sh
+npm install
+```
+
+## Development
+
+To start the project in development mode, run the following command:
+
+```sh
+npm run dev
+```
+
+This command will launch the development server, allowing you to see your changes in real-time.
+
+## Build
+
+To create a minified version of the package for production, execute:
+
+```sh
+npm run build
+```
+
+This will optimize your package for deployment, ensuring better performance.
---
+++ template-default/vite.config.ts
@@ -0,0 +1,57 @@
+import { htmlplus } from '@htmlplus/element/bundlers/vite.js';
+import path from 'path';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+import plugins from './htmlplus.config.js';
+
+export default defineConfig({
+  server: {
+    open: true
+  },
+  plugins: [
+    htmlplus(...plugins),
+    peerDepsExternal(),
+    dts({
+      outDir: 'dist/types',
+      resolvers: [
+        /**
+         * This resolver generates `.d.ts` files for each `.tsx` file.
+         * While this approach is somewhat of a workaround and temporary,
+         * we are actively seeking a more robust solution.
+         */
+        {
+          name: 'attach-dynamic-typing',
+          supports: (id) => id.endsWith('.tsx'),
+          transform({ root, id, code, program }) {
+            const sourceFile = program.getSourceFile(id)!;
+
+            let output = '';
+
+            program.emit(sourceFile, (fileName, contents) => {
+              output = contents + code.split('THE FOLLOWING TYPES HAVE BEEN ADDED AUTOMATICALLY').pop() || '';
+            });
+
+            return [
+              {
+                content: output,
+                path: path.relative(root, id.replace(/\.tsx?$/, '.d.ts'))
+              }
+            ];
+          }
+        }
+      ]
+    })
+  ],
+  build: {
+    emptyOutDir: false,
+    minify: false,
+    lib: {
+      formats: ['es'],
+      entry: {
+        'my-counter': 'src/my-counter.tsx'
+      }
+    }
+  }
+});
