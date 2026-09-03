adc61095a350a6dd882510f87e2320b40ebf7f08
Masood Abdolian <m.abdolian@gmail.com>
2026-09-03 20:41:08 +0330

    fix: Remove dts transform

--- template-default/vite.config.ts
+++ template-default/vite.config.ts
@@ -1,5 +1,3 @@
-import path from 'node:path';
-
 import { vite as htmlplus } from '@htmlplus/element/bundlers.js';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import { defineConfig } from 'vite';
@@ -9,42 +7,7 @@ export default defineConfig({
 	server: {
 		open: true
 	},
-	plugins: [
-		htmlplus(),
-		peerDepsExternal(),
-		dts({
-			outDir: 'dist/types',
-			resolvers: [
-				/**
-				 * This resolver generates `.d.ts` files for each `.tsx` file.
-				 * While this approach is somewhat of a workaround and temporary,
-				 * we are actively seeking a more robust solution.
-				 */
-				{
-					name: 'attach-dynamic-typing',
-					supports: (id) => id.endsWith('.tsx'),
-					transform({ root, id, code, program }) {
-						const sourceFile = program.getSourceFile(id);
-
-						let output = '';
-
-						program.emit(sourceFile, (_fileName, contents) => {
-							output =
-								contents + code.split('THE FOLLOWING TYPES HAVE BEEN ADDED AUTOMATICALLY').pop() ||
-								'';
-						});
-
-						return [
-							{
-								content: output,
-								path: path.relative(root, id.replace(/\.tsx?$/, '.d.ts'))
-							}
-						];
-					}
-				}
-			]
-		})
-	],
+	plugins: [htmlplus(), peerDepsExternal(), dts({ outDir: 'dist/types' })],
 	build: {
 		emptyOutDir: false,
 		minify: false,
